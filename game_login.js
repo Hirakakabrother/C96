de6754e845a236cf25cb1ac6b6efcdb74729bcd6
Hirakakabrother <108882055+Hirakakabrother@users.noreply.github.com>
2022-08-22 21:56:55 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,14 @@
+
+function addUser() {
+  player1_name = document.getElementById("player1_name_input").value;
+  player2_name = document.getElementById("player2_name_input").value;
+
+    //Set "player1_name" using localStorage.setItem() function
+	  //Set "player2_name" using localStorage.setItem() function
+    logalStorage.setItem("player_1").html;
+    logalStorage.setItem("player_2").html;
+    
+
+    window.location = "game_page.html";
+}
+
